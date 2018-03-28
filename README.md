40251e5236ed53ac6acb3f2661d88ae32ecd1d07
Daniel Ruf <daniel.ruf@ueberbit.de>
2018-03-28 15:55:05 +0200

    add readme and info output, clean up

---
+++ README.md
@@ -0,0 +1,11 @@
+# website-stack
+
+`website-stack` checks the frontend stack of websites.
+
+## Installation
+
+`npm i -g website-stack`
+
+## Usage
+
+`website-stack https://example.com`
--- index.js
+++ index.js
@@ -1,55 +1,54 @@
-const puppeteer = require('puppeteer');
-const chalk = require('chalk');
+const puppeteer = require('puppeteer')
+const chalk = require('chalk')
+const pkg = require('./package.json')
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+console.log(pkg.name + " " + pkg.version)
+
+run()
+
+async function run(){
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
   if(!process.argv[2]) {
-    console.log(chalk.red('No website was provided.'));
-    process.exit(1);
-    return;
+    console.log(chalk.red('No website was provided.'))
+    process.exit(1)
+    return
   }
 
-  await page.goto(process.argv[2]); // get website from the first parameter after the script
+  await page.goto(process.argv[2]) // get website from the first parameter after the script
 
-  let foundationVersion;
-  let jqueryVersion;
-  let bootstrapVersion;
+  let foundationVersion
+  let jqueryVersion
+  let bootstrapVersion
 
   try {
     foundationVersion = await page.evaluate('Foundation.version')
-  } catch(err){
-
-  };
+  } catch(err){}
 
   try {
     jqueryVersion = await page.evaluate('jQuery.fn.jquery')
     jqueryVersion = jqueryVersion.split(/ (.+)/)
     jqueryVersion = jqueryVersion[1]
                     ? `${jqueryVersion[0]} (${jqueryVersion[1]})`
-                    : jqueryVersion;
-  } catch(err){
-
-  };
+                    : jqueryVersion
+  } catch(err){}
 
   try {
     bootstrapVersion = await page.evaluate('bootstrap.Tooltip.VERSION || $.fn.tooltip.Constructor.VERSION')
-  } catch(err){
-
-  };
+  } catch(err){}
 
   typeof foundationVersion !== 'undefined'
          ? console.info(chalk.green(`Found Foundation ${chalk.yellow(foundationVersion)}`))
-         : console.log(chalk.red('No Foundation version found / loaded.'));
+         : console.log(chalk.red('No Foundation version found / loaded.'))
 
   typeof jqueryVersion !== 'undefined'
          ? console.info(chalk.green(`Found jQuery ${chalk.yellow(jqueryVersion)}`))
-         : console.log(chalk.red('No jQuery version found / loaded.'));
+         : console.log(chalk.red('No jQuery version found / loaded.'))
 
   typeof bootstrapVersion !== 'undefined'
          ? console.info(chalk.green(`Found Bootstrap ${chalk.yellow(bootstrapVersion)}`))
-         : console.log(chalk.red('No Bootstrap version found / loaded.'));
+         : console.log(chalk.red('No Bootstrap version found / loaded.'))
 
-  await browser.close();
-})();
+  await browser.close()
+}
