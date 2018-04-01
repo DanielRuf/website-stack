ea45ab48b7b2a10f269d683bf9d99b18634e0fc7
Daniel Ruf <daniel@daniel-ruf.de>
2018-04-01 02:27:58 +0200

    add some more checks

--- index.js
+++ index.js
@@ -22,6 +22,16 @@ async function run(){
   let jqueryVersion
   let bootstrapVersion
 
+  let webpackUsed
+  let googleanalyticsUsed
+  let isotopeUsed
+  let masonryUsed
+  let outlayerUsed
+  let packeryUsed
+  let googlewebfontsUsed
+  let wpUsed
+  let youtubeUsed
+
   try {
     foundationVersion = await page.evaluate('Foundation.version')
   } catch(err){}
@@ -38,17 +48,89 @@ async function run(){
     bootstrapVersion = await page.evaluate('bootstrap.Tooltip.VERSION || $.fn.tooltip.Constructor.VERSION')
   } catch(err){}
 
+  try {
+    webpackUsed = await page.evaluate('webpackJsonp')
+  } catch(err){}
+
+  try {
+    googleanalyticsUsed = await page.evaluate('GoogleAnalyticsObject || ga')
+  } catch(err){}
+
+  try {
+    isotopeUsed = await page.evaluate('Isotope')
+  } catch(err){}
+
+  try {
+    masonryUsed = await page.evaluate('Masonry')
+  } catch(err){}
+
+  try {
+    outlayerUsed = await page.evaluate('Outlayer')
+  } catch(err){}
+
+  try {
+    packeryUsed = await page.evaluate('Packery')
+  } catch(err){}
+
+  try {
+    googlewebfontsUsed = await page.evaluate('WebFontConfig && WebFontConfig.google')
+  } catch(err){}
+
+  try {
+    wpUsed = await page.evaluate('wp')
+  } catch(err){}
+
+  try {
+    youtubeUsed = await page.evaluate('YT')
+  } catch(err){}
+
   typeof foundationVersion !== 'undefined'
-         ? console.info(chalk.green(`Found Foundation ${chalk.yellow(foundationVersion)}`))
-         : console.log(chalk.red('No Foundation version found / loaded.'))
+         ? console.info(chalk.green(`Found Foundation ${chalk.yellow(foundationVersion)}.`))
+         : console.log(chalk.red('No Foundation found.'))
 
   typeof jqueryVersion !== 'undefined'
-         ? console.info(chalk.green(`Found jQuery ${chalk.yellow(jqueryVersion)}`))
-         : console.log(chalk.red('No jQuery version found / loaded.'))
+         ? console.info(chalk.green(`Found jQuery ${chalk.yellow(jqueryVersion)}.`))
+         : console.log(chalk.red('No jQuery found.'))
 
   typeof bootstrapVersion !== 'undefined'
-         ? console.info(chalk.green(`Found Bootstrap ${chalk.yellow(bootstrapVersion)}`))
-         : console.log(chalk.red('No Bootstrap version found / loaded.'))
+         ? console.info(chalk.green(`Found Bootstrap ${chalk.yellow(bootstrapVersion)}.`))
+         : console.log(chalk.red('No Bootstrap found.'))
+
+  typeof webpackUsed !== 'undefined'
+         ? console.info(chalk.green(`Found webpack.`))
+         : console.log(chalk.red('No webpack found.'))
+
+  typeof googleanalyticsUsed !== 'undefined'
+         ? console.info(chalk.green(`Found Google Analytics.`))
+         : console.log(chalk.red('No Google Analytics found.'))
+
+  typeof isotopeUsed !== 'undefined'
+         ? console.info(chalk.green(`Found Isotope.`))
+         : console.log(chalk.red('No Isotope found.'))
+
+  typeof mansonryUsed !== 'undefined'
+         ? console.info(chalk.green(`Found Masonry.`))
+         : console.log(chalk.red('No Masonry found.'))
+
+  typeof outlayerUsed !== 'undefined'
+         ? console.info(chalk.green(`Found Outlayer.`))
+         : console.log(chalk.red('No Outlayer found.'))
+
+  typeof packeryUsed !== 'undefined'
+         ? console.info(chalk.green(`Found Packery.`))
+         : console.log(chalk.red('No Packery found.'))
+
+  typeof googlewebfontsUsed !== 'undefined'
+         ? console.info(chalk.green(`Found Google Webfonts.`))
+         : console.log(chalk.red('No Google Webfonts found.'))
+
+  typeof wpUsed !== 'undefined'
+         ? console.info(chalk.green(`Found WordPress.`))
+         : console.log(chalk.red('No WordPress found.'))
+
+  typeof youtubeUsed !== 'undefined'
+         ? console.info(chalk.green(`Found YouTube.`))
+         : console.log(chalk.red('No YouTube found.'))
 
   await browser.close()
 }
